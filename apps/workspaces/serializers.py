611b5f0561978c90ee096a782692525a2f6232c8
ahmad <ahmad.im.iseed@gmail.com>
2026-06-29 23:43:25 +0300

    edited a workspace problem

---
+++ apps/workspaces/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Workspace, WorkspaceMember
+
+
+class WorkspaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workspace
+        fields = ["id", "name", "description", "created_by", "updated_at",
+                  "created_at"]
+        read_only_fields = ["id", "created_by", "updated_at", "created_at"]
+
+    def create(self, validated_data):
+            user = self.context['request'].user
+            workspace = Workspace.objects.create(created_by=user, **validated_data)
+            WorkspaceMember.objects.create(user=user, workspace=workspace, role=WorkspaceMember.Role.OWNER)
+            return workspace
+
+
+class WorkspaceMemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkspaceMember
+        fields = ["id", "user", "workspace", "role", "joined_at"]
+        read_only_fields = ["id", "joined_at"]
+
--- apps/workspaces/views.py
+++ apps/workspaces/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, permissions
+from .models import Workspace, WorkspaceMember
+from .serializers import WorkspaceSerializer, WorkspaceMemberSerializer
 # Create your views here.
+
+
+class WorkspaceViewSet(viewsets.ModelViewSet):
+
+    serializer_class = WorkspaceSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Workspace.objects.filter(memberships__user=self.request.user)
+
+
+class WorkspaceMemberViewSet(viewsets.ModelViewSet):
+    serializer_class = WorkspaceMemberSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return WorkspaceMember.objects.filter(workspace__memberships__user=self.request.user)
+    # الفكرة هون واحدة واحدة بدنا كل المستخدمين وين المستخدم لهاد مكان العمل هو اللي بعت الطلب عالمتصفح ال ايه بي اي
