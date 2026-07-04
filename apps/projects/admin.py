cf468abfa11f01cd7c01e91bd72937dd37387117
ahmad <ahmad.im.iseed@gmail.com>
2026-07-04 13:18:08 +0300

    added projects schema

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -4,4 +4,4 @@ from django.contrib import admin
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'workspace', 'created_by', 'created_at', 'updated_at')
     search_fields = ('name', 'description')
-    list_filter = ('workspace', 'created_by', 'created_at', 'updated_at')
+    list_filter = ('workspace', 'created_at', 'updated_at')
